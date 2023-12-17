004211614d0b05d5545f9782996528f93ae585b8
Mr-Vitalii <co4ckowoi.v@gmail.com>
2023-12-17 16:26:56 +0200

    created contact-us section

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from '@/common/types'
 import { Home } from '@/components/Home/Home'
 import { Benefits } from '@/components/Benefits/Benefits'
 import { OurClasses } from '@/components/OurClasses/OurClasses'
+import { ContactUs } from '@/components/ContactUs/ContactUs'
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -32,6 +33,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
+      <ContactUs setSelectedPage={setSelectedPage} />
     </div>
   )
 }
